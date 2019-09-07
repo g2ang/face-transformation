333fd5168452785f14ce5e729c4fec5fe84ca117
Taehwan Noh <taehwanno.dev@gmail.com>
2019-09-07 13:52:02 +0900

    Add ThemeProvider to all storybooks

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,16 @@
-import { configure, setAddon } from '@storybook/react';
+import React from 'react';
+import { addDecorator, configure, setAddon } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
 import { withOptions } from '@storybook/addon-options';
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../src/theme';
 
 setAddon(infoAddon);
 
 const req = require.context('../src', true, /\.stories\.tsx$/);
+
+addDecorator(storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>);
 
 withOptions({
   name: 'Face Transformation',

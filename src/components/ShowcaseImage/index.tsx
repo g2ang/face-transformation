8837eb01bd498e197a3407a2bec9ce9540dbbad2
Taehwan Noh <taehwanno.dev@gmail.com>
2019-09-17 22:32:17 +0900

    Update ShowcaseImage style

--- src/components/ShowcaseImage/index.tsx
+++ src/components/ShowcaseImage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import Image from 'components/Image';
 import PropsType from 'components/PropsType';
@@ -12,14 +13,23 @@ const propTypes = {
 
 type ShowcaseImageProps = PropsType<typeof propTypes>;
 
+const BlurredImage = styled(Image)`
+  transition: opacity 0.8s;
+  :hover {
+    opacity: 0.4;
+  }
+`;
+
 const ShowcaseImage: React.FC<ShowcaseImageProps> = ({ id, src, onClick }) => (
-  <Image
-    height="100%"
-    src={src}
-    onClick={() => {
-      onClick(id);
-    }}
-  />
+  <a href={src} download target="_blank" rel="noopener noreferrer">
+    <BlurredImage
+      height="100%"
+      src={src}
+      onClick={() => {
+        onClick(id);
+      }}
+    />
+  </a>
 );
 
 ShowcaseImage.propTypes = propTypes;

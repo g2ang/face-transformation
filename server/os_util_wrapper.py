df64df9a3f1bdcdcf5c011620aed2695d7abef15
extJo <josungyun6518@gmail.com>
2019-09-24 16:35:40 +0900

    fix save_temp_img function

--- server/os_util_wrapper.py
+++ server/os_util_wrapper.py
@@ -21,6 +21,5 @@ def remove_all_file_at_directory(directory):
           print(e)
 
 def save_temp_img(img):
-    name = secure_filename(img.filename)
-    img.save(f"{current_path}/raw_temp_img", name)
+    img.save(f"{current_path}/raw_temp_img", img.name)
     return f"{current_path}/raw_temp_img/{name}"

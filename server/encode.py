84b9774c711a11fca6319f26f0c8b5b59db300a4
extJo <josungyun6518@gmail.com>
2019-09-24 20:54:06 +0900

    replace os.system to subprocess

--- server/encode.py
+++ server/encode.py
@@ -1,5 +1,6 @@
 import os
 from os.path import join
+import subprocess
 import os_util_wrapper
 import pickle
 from PIL import Image
@@ -39,21 +40,30 @@ def convert_style(latent_vector, direction, coeffs):
 
 def generate_latent_vector():
     align_image_script_path = f"{os_util_wrapper.parent_path}/encoder/align_images.py"
-    align_image_script_command = f"python {align_image_script_path} {os_util_wrapper.parent_path}/server/raw_temp_img {os_util_wrapper.parent_path}/server/aligned_temp_img"
-    os.system(align_image_script_command)
+    raw_temp_img_path = f"{os_util_wrapper.parent_path}/server/raw_temp_img"
+    aligned_temp_img_path = f"{os_util_wrapper.parent_path}/server/aligned_temp_img"
+    
+    subprocess.call(
+      [
+        "python", align_image_script_path, raw_temp_img_path, aligned_temp_img_path
+      ]
+    )
 
     encode_images_script_path = f"{os_util_wrapper.parent_path}/encoder/encode_images.py"
-    encode_images_script_command = f"python {encode_images_script_path} {os_util_wrapper.parent_path}/server/aligned_temp_img {os_util_wrapper.parent_path}/server/generated_temp_img {os_util_wrapper.parent_path}/server/temp_latent_representations"
-    os.system(encode_images_script_command)
+    generated_temp_img_path = f"{os_util_wrapper.parent_path}/server/generated_temp_img"
+    temp_latent_representations_path = f"{os_util_wrapper.parent_path}/server/temp_latent_representations"
+    
+    subprocess.call(
+        [
+          "python", encode_images_script_path, aligned_temp_img_path, generated_temp_img_path, temp_latent_representations_path
+        ]
+    )
 
 def load_generated_latent_vector():
     for file in os.listdir(f"{os_util_wrapper.parent_path}/server/temp_latent_representations"):
         if file.endswith(".npy"):
-            return np.load(join(f"{os_util_wrapper.parent_path}/server/temp_latent_representations", file))
+            return np.load(f"{os_util_wrapper.parent_path}/server/temp_latent_representations/{file}")
 
 smile_direction = np.load(join(PROJ_PATH, 'ffhq_dataset/latent_directions/smile.npy'))
 gender_direction = np.load(join(PROJ_PATH, 'ffhq_dataset/latent_directions/gender.npy'))
 age_direction = np.load(join(PROJ_PATH, 'ffhq_dataset/latent_directions/age.npy'))
-
-man_latent_vector = np.load(join(PROJ_PATH, 'ffhq_dataset/latent_representations/donald_trump_01.npy'))
-woman_latent_vector = np.load(join(PROJ_PATH, 'ffhq_dataset/latent_representations/hillary_clinton_01.npy'))

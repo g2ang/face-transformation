a1d370f271b351a68bcd9adfbcd5bd679903a3a1
extJo <josungyun6518@gmail.com>
2019-09-24 16:22:01 +0900

    create os util

---
+++ server/os_uitl.py
@@ -0,0 +1,26 @@
+import os 
+import shutil
+
+current_path = os.getcwd()
+
+def remove_file_relative_path(file_name):
+    os.remove(f'{current_path}/{file_name}')
+
+def remove_file_absolute_path(path):
+    os.remove(path)
+
+def remove_all_file_at_directory(directory):
+    folder = f"{current_path}/{directory}"
+
+    for the_file in os.listdir(folder):
+      file_path = os.path.join(folder, the_file)
+      try:
+          if os.path.isfile(file_path):
+              os.unlink(file_path)
+      except Exception as e:
+          print(e)
+
+def save_temp_img(img):
+    name = secure_filename(img.filename)
+    img.save(f"{current_path}/raw_temp_img", name)
+    return f"{current_path}/raw_temp_img/{name}"
